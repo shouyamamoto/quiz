ac84b7b293df5cc0248e3a6eda679c418748a115
yamamoto syo <syo19961113@gmail.com>
2020-12-09 23:16:46 +0900

    fin wuiz

--- js/main.js
+++ js/main.js
@@ -4,14 +4,17 @@
   const question = document.getElementById('question');
   const choices = document.getElementById('choices');
   const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+  const scoreLabel = document.querySelector('#result > p');
 
-  const quizSet = [
-      {q: 'What is A?', c: ['A0', 'A1', 'A2']},
-      {q: 'What is B?', c: ['B0', 'B1', 'B2']},
-      {q: 'What is C?', c: ['C0', 'C1', 'C2']},
-  ];
+  const quizSet = shuffle([
+    {q: '世界で一番大きい湖は？', c: ['カスピ海', 'カリブ海', '琵琶湖']},
+    {q: '2の8乗は？', c: ['256', '64', '1024']},
+    {q: '次のうち最初にリリースされた言語は?', c: ['Python', 'JavaScript', 'HTML']},
+]);
   let currentNum = 0;
   let isAnswered;
+  let score = 0;
   
 
   function shuffle(arr) {
@@ -38,6 +41,7 @@
 
     if (li.textContent === quizSet[currentNum].c[0]) {
         li.classList.add('correct');
+        score++;
     } else {
         li.classList.add('wrong');
     }
@@ -77,7 +81,12 @@
       }
       btn.classList.add('disabled');
 
-      currentNum++;
-      setQuiz();
-  });
+      if (currentNum === quizSet.length - 1) {
+        scoreLabel.textContent = `Score: ${score} / ${quizSet.length}`;
+        result.classList.remove('hidden');
+      } else {
+        currentNum++;
+        setQuiz();
+    }
+});
 }
